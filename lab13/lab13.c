77f9f3fbf2e835324a6d8be6ff162c63c9de2535
Sveta-Pam-Pam <svetamashenko@yandex.ru>
2019-12-24 14:27:16 +0300

    lab13

--- lab13/lab13.c
+++ lab13/lab13.c
@@ -1,65 +1,86 @@
 #include <stdio.h>
 #include <math.h>
 
-unsigned long long int proofer (unsigned long long int a, long long unsigned int b)
+unsigned long long int proofer(unsigned long long int a, long long unsigned int b)
 {
-    unsigned long long int answer = 100000000000000;
     int k = 0;
-    for (int i = 1; i <= 15; i++)
+    for (int i = 1; i <= 12; i++)
     {
         if (((a % 10) == 1) & ((b % 10) == 1))
         {
-            k = 1;
             return 1;
         }
-        a = a / 10; b = b / 10;}
-        return k;
+        a = a / 10;
+        b = b / 10;
     }
+    return 0;
+}
 
-    unsigned long long int add1(int a)
+unsigned long long int add1(int a)
+{
+    unsigned long long int first = 1000000000000;
+    if ((a <= 108) && (a >= 97))
     {
-        unsigned long long int first = 1000000000000000;
-        if ((a <= 110) && (a >= 97))
-        {
-            first = first + pow(10, 111 - a);
-        }
-        return first;
+        first = first + pow(10, 108 - a);
     }
+    return first;
+}
 
-    unsigned long long int add2(int a)
+unsigned long long int add2(int a)
+{
+    unsigned long long int first = 100000000000000;
+    if ((a <= 122) && (a >= 109))
     {
-        unsigned long long int first = 100000000000;
-        if ((a <= 122) && (a >= 111))
-        {
-            first = first + pow(10, 122 - a);
-        }
-        return first;
+        first = first + pow(10, 122 - a);
     }
+    return first;
+}
 
 int main()
 {
     char word;
     int count = 0, trigger = 0, k = 0;
-    unsigned long long int letter1 = 1100010001000001, letter2 = 100000100010;
+    char premier, now, last;
+    unsigned long long int letter1 = 1100010001000, letter2 = 100100000100010;
     while (scanf("%c", &word) != EOF)
     {
-        if ((trigger == 0) & (word != ' '))
+        if (trigger == 2)
         {
-            int premier = word;
-            trigger = 1;
-            if ((trigger == 1) & (word != ' ')){
-            k++;
-            for (int i = 1; i < k - 1; i++)
+
+            if ((proofer(add1(premier), letter1) == 1) || (proofer(add2(premier), letter2) == 1))
             {
-                int last = word;
-                if (proofer(premier, last) == 1)
+                if ((proofer(add1(last), letter1) == 1) || (proofer(add2(last), letter2) == 1))
                 {
                     count++;
                 }
-                trigger = 0;
-            }}
+            }
+            trigger = 0;
+        }
+
+        else if ((trigger == 1) && (word == ' '))
+        {
+            last = now;
+            trigger = 2;
+        }
+
+        else if ((trigger == 1) && (word != ' '))
+        {
+            last = now;
+            now = word;
+        }
+
+        if ((trigger == 0) && (word == ' '))
+        {
+            premier = word;
+        }
+        if ((trigger == 0) && (word != ' '))
+        {
+            premier = word;
+            now = word;
+            trigger = 1;
         }
     }
+
     if (count > 0)
     {
         printf("%s", "yes");
--- lab13/tester.c
+++ lab13/tester.c
@@ -1,56 +1,56 @@
 #include <stdio.h>
 #include <math.h>
 
-unsigned long long int proofer (unsigned long long int a, long long unsigned int b)
+unsigned long long int proofer(unsigned long long int a, long long unsigned int b)
 {
-    unsigned long long int answer = 100000000000000;
     int k = 0;
-    for (int i = 1; i <= 15; i++)
+    for (int i = 1; i <= 13; i++)
     {
-        if (((a % 10) == 1) & ((b % 10) == 1))
+        if (((a % 10) == 1) && ((b % 10) == 1))
         {
             k = 1;
-            return 1;
         }
-        a = a / 10; b = b / 10;}
-        return k;
+        a = a / 10;
+        b = b / 10;
     }
+    return k;
+}
 
-    unsigned long long int add1(int a)
+unsigned long long int add1(int a)
+{
+    unsigned long long int first = 1000000000000;
+    if ((a <= 108) && (a >= 97))
     {
-        unsigned long long int first = 1000000000000000;
-        if ((a <= 110) && (a >= 97))
-        {
-            first = first + pow(10, 111 - a);
-        }
-        return first;
+        first = first + pow(10, 108 - a);
     }
+    return first;
+}
 
-    unsigned long long int add2(int a)
+unsigned long long int add2(int a)
+{
+    unsigned long long int first = 100000000000000;
+    if ((a <= 122) && (a >= 109))
     {
-        unsigned long long int first = 100000000000;
-        if ((a <= 122) && (a >= 111))
-        {
-            first = first + pow(10, 122 - a);
-        }
-        return first;
+        first = first + pow(10, 122 - a);
     }
+    return first;
+}
 
-    int main()
+int main()
+{
+    int count = 0;
+    char a, b;
+    scanf("%c %c", &a, &b);
+    int word1 = a;
+    int word2 = b;
+    unsigned long long int letter1 = 1100010001000, letter2 = 100100000100010;
+    if ((proofer(add1(a), letter1) == 1) || (proofer(add2(a), letter2) == 1))
     {
-        int k = 0;
-        char a, b;
-        scanf ("%c%c", &a, &b);
-        int word1 = a; int word2 = b;
-        unsigned long long int letter1 = 1100010001000001, letter2 = 100000100010;
-        if ((proofer(add1(word1), letter1) == 1) || (proofer(add2(word1), letter2) == 1))
+        if ((proofer(add1(b), letter1) == 1) || (proofer(add2(b), letter2) == 1))
         {
-            k = k + 50;
+            count++;
         }
-        if ((proofer(add1(word2), letter1) == 1) || (proofer(add2(word2), letter2) == 1))
-        {
-            k = k + 100;
-        }
-        printf("%llu %llu %llu %llu %d",add1(a), add2(b), proofer(add1(word1), letter1), proofer(add2(word2), letter2), k);
-        return 1;
     }
+    printf("%llu %llu %llu %llu %d", add1(a), add2(b), proofer(add1(word1), letter1), proofer(add2(word2), letter2), count);
+    return 1;
+}
