74608926975e775e51a4de29ee007350a6400581
Sveta-Pam-Pam <svetamashenko@yandex.ru>
2020-05-27 18:40:44 +0300

    Release

--- 2 semestr/lab25-26+/sort/sort.c
+++ 2 semestr/lab25-26+/sort/sort.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-#include <malloc.h>
 #include "sort.h"
-#include "../push_pop/push_pop.h"
 
 void sort(int *stack, int first, int count)
 {
-    int *stnew[count];
-    int n = 0;
+    int *stnew;
     int i = first;
     int j = count;
+    int o;
     int x = stack[(first + j) / 2];
     do
     {
         while (stack[i] < x)
-        {
             i++;
-        }
         while (stack[j] > x)
-            for (int k = 0; k < j; k++)
-                *stnew = *push(stack[i], stnew[k], n);
-        *stack = *pop(stack[i], count - 1);
-        j--;
-
+            j--;
         if (i <= j)
         {
             if (stack[i] > stack[j])
